8017c9c58212982c5b5820b27982005bffefbdd9
ivanadob <ivana.dob@gmail.com>
2024-09-18 11:19:59 +0200

    add single view for ms

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,18 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: { brand: "#ebcb00" },
+      colors: {
+        brandBlack: "2e0e02",
+        brandBrown: "#581908",
+        brand: "#cd4835",
+        brand50: "#fdf4f3",
+        brand100: "#fce7e4",
+        brand200: "#fad3ce",
+        brand500: "#e16452",
+        brand600: "#cd4835",
+        brand900: "#772f25",
+        brand950: "#40150f",
+      },
       backgroundImage: {
         jerusalem: "url('title-image.png')",
       },
