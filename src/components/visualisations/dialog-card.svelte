416e7c87d1f3264b9bec8ac9e7d09f0e23f8bf45
Ivana Dobcheva <ivana.dob@gmail.com>
2026-03-16 14:54:50 +0100

    interesting in firefox modal works in chrome not

---
+++ src/components/visualisations/dialog-card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { selectedJadId } from "@/stores/jad_store";
+import PassageCard from "@/components/passage-card.svelte";
+import { onMount } from "svelte";
+
+let dialog: HTMLDialogElement;
+
+$: if ($selectedJadId && dialog && !dialog.open) {
+  dialog.showModal();
+}
+</script>
+
+<dialog id="passage-dialog" bind:this={dialog} class="w-1/4 border-2 border-brand-800 
+grid space-y-2 rounded-md pb-3 bg-brand-100">
+    <PassageCard/>
+   <button commandfor="passage-dialog" command="close" 
+   class="block mx-auto text-lg bg-brand-50 px-3 py-1.5 border border-neutral-300 
+   rounded-md font-bold text-brand-800 cursor-pointer hover:text-brand-500">Close</button>
+</dialog>
